10ee8d9942b23db157b60aee712edc1735cac9a6
pjt3591oo <pjt3591oo@gmail.com>
2021-12-20 09:18:56 +0900

    socket.js 어댑터 주석 추가

--- socket.js
+++ socket.js
@@ -3,6 +3,10 @@ const socket = require("socket.io");
 function init(httpServer) {
 
   const io = socket(httpServer);
+  
+  // adapter configuration
+  // io.adapter(require("socket.io-redis")({ host: "localhost", port: 6379 }));
+
   io.on('connection', (socket) => {
     console.log('connected socketId: ', socket.id);
     socket.on('ping', data => {
